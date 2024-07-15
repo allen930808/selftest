aaf8910995dd794222387f5df67ea9e96346aed4
allen930808 <winny930808@gmail.com>
2024-07-15 23:33:43 +0800

    first try

--- main.c
+++ main.c
@@ -1,12 +1,59 @@
 #include <stdio.h>
+int main() {
+    static double a, b;
+    char count;
+    int i = 0;
+    double final = 0;
 
-int add(const int a, const int b) {
-    return a+b;
+    printf("Enter two first and second operand: ");
+    scanf("%lf %lf", &a, &b);
+
+    while(1) {
+        if(i > 0) {
+            printf("Enter new oprand: ");
+            scanf("%lf", &b);
+        }
+        printf("Enter an operator (+, -, *, /), "
+               "if want to exit press x: ");
+        while (getchar() != '\n') {}
+        scanf("%c", &count);
+
+        if(count == 'x')
+            break;
+
+        switch(count) {
+
+            case '+':
+                printf("%.1lf + %.1lf = %.1lf\n", a, b, a + b);
+            final = a + b;
+            break;
+            case '-':
+                printf("%.1lf - %.1lf = %.1lf\n", a, b, a - b);
+            final = a - b;
+            break;
+            case '*':
+                printf("%.1lf * %.1lf = %.1lf\n", a, b, a * b);
+            final = a * b;
+            break;
+            case '/':
+                if (b != 0) {
+                    printf("%.1lf / %.1lf = %.1lf\n", a, b, a / b);
+                    final = a / b;
+                } else {
+                    printf("Error: Division by zero is not allowed.\n");
+                    continue;
+                }
+            break;
+
+            default:
+                printf("Error, Invalid operand\n");
+            continue;
+        }
+        a = final;
+        i++;
+        printf("\n");
+    }
+
+    printf("Total iteration = %d. Final result = %lf", i + 1, final);
+
 }
-int main(void) {
-    const int a = 5;
-    const int b = 9;
-    printf("%d\n", add(a, b));
-}
-
-
